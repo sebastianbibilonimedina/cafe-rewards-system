cdc71d70beaa8e9f9c7e981a3dc14ae91a1ff305
sebabibiloni <sebastian.bibiloni@upr.edu>
2024-03-13 22:16:53 -0400

    Updated environment variables and added new dependencies

--- cafe-rewards-backend/app.js
+++ cafe-rewards-backend/app.js
@@ -1,8 +1,31 @@
+require('dotenv').config();
 const express = require('express');
-const userRoutes = require('./routes/users');
+const { Pool } = require('pg');
 const app = express();
 
-app.use(express.json());
-app.use('/api/users', userRoutes);
+const pool = new Pool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_DATABASE,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+});
 
-app.listen(3000, () => console.log('Server is running on port 3000'));
+app.get('/', (req, res) => {
+    res.send('Café Rewards PR Backend is running...');
+});
+
+// Test database connection
+app.get('/testdb', async (req, res) => {
+    try {
+        const testResult = await pool.query('SELECT NOW()');
+        res.json(testResult.rows[0]);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});

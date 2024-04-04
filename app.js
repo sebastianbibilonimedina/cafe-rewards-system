282540e4575143fc00967d559e00db9ffc73deb9
sebas <sebastian.bibiloni@upr.edu>
2024-04-04 04:49:50 -0400

    Refactor app.js to simplify and structure code
    The commit organizes existing code in app.js into functions for better readability and maintainability. The changes include modularizing middleware configuration, database connection, and route loading into separate functions. It also adjusts the way static files are served.

--- app.js
+++ app.js
@@ -3,62 +3,61 @@ const express = require('express');
 const cors = require('cors');
 const path = require('path');
 const db = require('./server');
-
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
-// Added static middleware to serve frontend static files
-app.use(express.static(path.join(__dirname, 'client/build')));
+const clientHtmlPath = path.join(__dirname, 'client', 'html');
 
-// Apply CORS middleware with configuration
-app.use(cors({
-    origin: process.env.CORS_ORIGIN // make sure this is set in your .env.local file
-}));
+function configureMiddleware() {
+    app.use(cors({
+        origin: process.env.CORS_ORIGIN // make sure this is set in your .env.local file
+    }));
+    app.use(express.json());
+}
 
-app.use(express.json());
+function connectDatabase() {
+    db.sequelize.authenticate()
+        .then(() => {
+            console.log('Connection has been established successfully.');
+            // Uncomment below if you want the tables to be created (and possibly dropped if they exist)
+            // return db.sequelize.sync();
+        })
+        .catch(err => {
+            console.error('Unable to connect to the database:', err);
+        });
+}
 
-db.sequelize.authenticate()
-    .then(() => {
-        console.log('Connection has been established successfully.');
-        // Uncomment below if you want the tables to be created (and possibly dropped if they exist)
-        // return db.sequelize.sync();
-    })
-    .catch(err => {
-        console.error('Unable to connect to the database:', err);
+function loadRoutes() {
+    const routes = ['users', 'rewards', 'transactions', 'orders', 'owners', 'menus', 'digitalwallets', 'coffeeshops'];
+
+    routes.forEach(route => {
+        let routePath = `./server/models-controllers-routes/${route}/${route}Router`;
+        app.use(`/api/${route}`, require(routePath));
     });
 
-const usersRoutes = require('./server/models-controllers-routes/users/usersRouter');
-const rewardsRoutes = require('./server/models-controllers-routes/rewards/rewardsRouter');
-const transactionsRoutes = require('./server/models-controllers-routes/transactions/transactionsRouter');
-const ordersRoutes = require('./server/models-controllers-routes/orders/ordersRouter');
-const ownersRoutes = require('./server/models-controllers-routes/owners/ownersRouter');
-const menusRoutes = require('./server/models-controllers-routes/menus/menusRouter');
-const digitalWalletsRoutes = require('./server/models-controllers-routes/digitalwallets/digitalwalletsRouter');
-const coffeeShopsRoutes = require('./server/models-controllers-routes/coffeeshops/coffeeshopsRouter');
+    // Error handling middleware should be last piece of middleware used
+    app.use(function (err, req, res, next) {
+        console.error(err.stack);
+        res.status(500).send('Something broke!');
+    });
+}
+
+// Serve static files from the 'client/html' directory
+app.use(express.static(clientHtmlPath));
+// When a GET request is made to the server, serve the 'index.html' file
+app.get('*', (req, res) => {
+    res.sendFile(path.join(clientHtmlPath, 'index.html'));
+});
 
 app.use((req, res, next) => {
     console.log(`A ${req.method} request received at ${new Date().toLocaleTimeString()} on ${req.url}`);
     next();
 });
 
-app.use('/api/users', usersRoutes);
-app.use('/api/rewards', rewardsRoutes);
-app.use('/api/transactions', transactionsRoutes);
-app.use('/api/orders', ordersRoutes);
-app.use('/api/owners', ownersRoutes);
-app.use('/api/menus', menusRoutes);
-app.use('/api/digitalwallets', digitalWalletsRoutes);
-app.use('/api/coffeeshops', coffeeShopsRoutes);
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname+'/client/build/index.html'));
-});
-
-// Error handling middleware should be last piece of middleware used
-app.use(function (err, req, res, next) {
-    console.error(err.stack);
-    res.status(500).send('Something broke!');
-});
+connectDatabase();
+configureMiddleware();
+loadRoutes();
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
